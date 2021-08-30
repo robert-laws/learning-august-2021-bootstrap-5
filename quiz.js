ab5fb546b73aa8956eec872f7ce9e3c89cb622fa
Robert Laws <roblaws@me.com>
2021-08-30 16:56:07 +0300

    begin set up for quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+let quiz = [
+  {
+    number: 1,
+    question: 'What is the capital of France?',
+    answers: ['Paris', 'London', 'Berlin', 'Madrid'],
+    correctAnswer: 'Paris',
+  },
+  {
+    number: 2,
+    question: 'What is the capital of Germany?',
+    answers: ['Paris', 'London', 'Berlin', 'Madrid'],
+    correctAnswer: 'Berlin',
+  },
+];
+
+export default quiz;
